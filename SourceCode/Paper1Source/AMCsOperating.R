55d2eb30095d8627a7d7294c0f9bdf982e3b9579
Christopher Gandrud <christopher.gandrud@gmail.com>
2012-12-14 09:47:54 +0900

    print data

--- SourceCode/Paper1Source/AMCsOperating.R
+++ SourceCode/Paper1Source/AMCsOperating.R
@@ -17,10 +17,12 @@ SumOp <- ddply(NotNaAMCType, .(year, AMCType), function(x) sum(x$NumAMCOpNoNA))
 SumOp <- subset(SumOp, AMCType !=  "?")
 SumOp <- subset(SumOp, AMCType !=  "None")
 
-ggplot(data = SumOp, aes(year, V1)) +
-        geom_vline(xintercept = c(1991, 1997, 2008), linetype = "dashed", size = 0.5) +
-        geom_line(aes(color = AMCType), size = 2, alpha = I(0.9)) +
-        scale_color_manual(values = TypeColors, name="") +
-        scale_x_continuous(limits = c(1980, 2011)) +
-        xlab("") + ylab("Number Operating\n") +
-        theme_bw(base_size = 15)
+OperatingPlot <- ggplot(data = SumOp, aes(year, V1)) +
+				        geom_vline(xintercept = c(1991, 1997, 2008), linetype = "dashed", size = 0.5) +
+				        geom_line(aes(color = AMCType), size = 2, alpha = I(0.9)) +
+				        scale_color_manual(values = TypeColors, name="") +
+				        scale_x_continuous(limits = c(1980, 2011)) +
+				        xlab("") + ylab("Number Operating\n") +
+				        theme_bw(base_size = 15)
+
+print(OperatingPlot)
