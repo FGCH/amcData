44e2d3a04d5fe48899582227c4a8c9d08ec3f284
Christopher Gandrud <christopher.gandrud@gmail.com>
2014-01-07 17:58:28 -1100

    polity added to analysis

--- SourceCode/Paper1Source/PaperSource5.R
+++ SourceCode/Paper1Source/PaperSource5.R
@@ -70,36 +70,36 @@ MA3 <- coxph(Surv(year1980, AMCAnyCreated) ~ SystemicCrisisLag3 + IMF.AMC +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MA4 <- coxph(Surv(year1980, AMCAnyCreated) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + UDS +
+               TotalReservesGDPLog + polity2 +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MA5 <- coxph(Surv(year1980, AMCAnyCreated) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + CvHOwnPerc + EconReScale + UDS + 
+               TotalReservesGDPLog + CvHOwnPerc + EconReScale + polity2 + 
                polariz + checks +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MA6 <- coxph(Surv(year1980, AMCAnyCreated) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + CvHOwnPerc + EconReScale + UDS + 
+               TotalReservesGDPLog + CvHOwnPerc + EconReScale + polity2 + 
                polariz*checks +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MA7 <- coxph(Surv(year1980, AMCAnyCreated) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + CvHOwnPerc + EconReScale + UDS + 
+               TotalReservesGDPLog + CvHOwnPerc + EconReScale + polity2 + 
                polariz*checks + as.factor(ElectionYear1) +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MA8 <- coxph(Surv(year1980, AMCAnyCreated) ~ SystemicCrisisLag3 +
-               TotalReservesGDPLog + CvHOwnPerc + EconReScale + UDS + 
+               TotalReservesGDPLog + CvHOwnPerc + EconReScale + polity2 + 
                polariz*checks + IMFDreherLag3 +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MA9 <- coxph(Surv(year1980, AMCAnyCreated) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + CvHOwnPerc + EconReScale + UDS + 
+               TotalReservesGDPLog + CvHOwnPerc + EconReScale + polity2 + 
                polariz*checks + GDPperCapitaLog +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data) 
 
 MA10 <- coxph(Surv(year1980, AMCAnyCreated) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + CvHOwnPerc + EconReScale + UDS + 
+               TotalReservesGDPLog + CvHOwnPerc + EconReScale + polity2 + 
                polariz*checks + GDPCurrentUSDLog + IMF.AMC +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data) 
 
@@ -119,36 +119,36 @@ MC3 <- coxph(Surv(year1980, AMCCent) ~ SystemicCrisisLag3 + IMF.AMC +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MC4 <- coxph(Surv(year1980, AMCCent) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + UDS +
+               TotalReservesGDPLog + polity2 +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MC5 <- coxph(Surv(year1980, AMCCent) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                polariz + checks +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MC6 <- coxph(Surv(year1980, AMCCent) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                polariz*checks +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MC7 <- coxph(Surv(year1980, AMCCent) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                as.factor(ElectionYear1) +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MC8 <- coxph(Surv(year1980, AMCCent) ~ SystemicCrisisLag3 + 
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                IMFDreherLag3 +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MC9 <- coxph(Surv(year1980, AMCCent) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                GDPperCapitaLog +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data) 
 
 MC10 <- coxph(Surv(year1980, AMCCent) ~ SystemicCrisisLag3 + IMF.AMC +
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                GDPCurrentUSDLog +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)  
 
@@ -168,41 +168,41 @@ MD3 <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MD4 <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 + 
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                polariz + checks +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MD5 <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 + 
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                polariz*checks +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MD6 <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 + 
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                as.factor(ElectionYear1) +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MD7 <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 + 
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                IMFDreherLag3 +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)
 
 MD8 <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 + 
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                GDPperCapita +
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data) 
 
 MD9 <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 + 
-               TotalReservesGDPLog + EconReScale + UDS + 
+               TotalReservesGDPLog + EconReScale + polity2 + 
                GDPCurrentUSDLog + 
                cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data)  
 
 MD10 <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 + 
-                TotalReservesGDPLog + EconReScale*UDS + 
+                TotalReservesGDPLog + EconReScale*polity2 + 
                 cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data) 
 
 MD10Plot <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 + 
-                TotalReservesGDPLog + UDS*EconReScale + 
+                TotalReservesGDPLog + polity2*EconReScale + 
                 cluster(imfcode) + strata(NumAMCCountryNoNA), data = Data) 
 
 ################
@@ -212,7 +212,7 @@ MD10Plot <- coxph(Surv(year1980, AMCDecent) ~ SystemicCrisisLag3 +
 ##################### Any AMC results tables ######################
 MANames <- c("A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10")
 CoefNamesMA <- c("Crisis 3 yr. Lag", "MONA IMF Condition", "Log Reserves/GDP", "Foreign Ownership", 
-                 "Economic Inst.", "UDS",
+                 "Economic Inst.", "Polity",
                  "Polarise", "Checks", "Polarise*Checks", "Executive Election",
                  "IMF Stand-By", "Log GDP/Capita", "Log Total GDP")
 
@@ -230,7 +230,7 @@ texreg(list(MA1, MA2, MA3, MA4, MA5, MA6, MA7, MA8, MA9, MA10),
 ##################### Centralised AMC results tables ######################
 MCNames <- c("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10")
 CoefNamesMC <- c("Crisis 3 yr. Lag", "MONA IMF Condition", "Log Reserves/GDP", "Foreign Ownership", 
-                 "Economic Inst.", "UDS",
+                 "Economic Inst.", "Polity",
                  "Polarise", "Checks", "Polarise*Checks", "Executive Election",
                  "IMF Stand-By", "Log GDP/Capita", "Log Total GDP")
 
@@ -249,9 +249,9 @@ texreg(list(MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8, MC9, MC10),
 ##################### DeCentralised AMC results tables ######################
 MDNames <- c("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10")
 CoefNamesMD <- c("Crisis 3 yr. Lag", "Log Reserves/GDP", "Foreign Ownership", 
-                 "Economic Inst.", "UDS",
+                 "Economic Inst.", "Polity",
                  "Polarise", "Checks", "Polarise*Checks", "Executive Election",
-                 "IMF Stand-By", "GDP/Capita", "Log Total GDP", "Econ. Inst.*UDS")
+                 "IMF Stand-By", "GDP/Capita", "Log Total GDP", "Econ. Inst.*Polity")
 
 texreg(list(MD1, MD2, MD3, MD4, MD5, MD6, MD7, MD8, MD9, MD10),
        custom.model.names = MDNames,
@@ -282,33 +282,25 @@ simGG(Sim2.1, ribbons = TRUE, alpha = 0.3, xlab = "\nEconomic Institutional Qual
 
 dev.off()
 
-##################### UDS Hazard Ratios Effect #########
+##################### Polity Hazard Ratios Effect #########
 ## Split up to give a better look at different ranges of fitted values.
 
-Sim3.1 <- coxsimLinear(MD9, b = "UDS", qi = "Hazard Ratio",
-                     Xj = seq(-2, 0, 0.05), ci = 0.95)
+Sim3.1 <- coxsimLinear(MD9, b = "polity2", qi = "Hazard Ratio",
+                     Xj = seq(-10, 0, 0.05), ci = 0.95)
 
-Sim3.2 <- coxsimLinear(MD9, b = "UDS", qi = "Hazard Ratio",
-                       Xj = seq(0, 1, 0.05), ci = 0.95)
+Sim3.2 <- coxsimLinear(MD9, b = "polity2", qi = "Hazard Ratio",
+                       Xj = seq(0, 10, 0.05), ci = 0.95)
 
-Sim3.3 <- coxsimLinear(MD9, b = "UDS", qi = "Hazard Ratio",
-                       Xj = seq(1, 2, 0.05), ci = 0.95)
 
 ######## Maybe remove if econ qual/democracy interaction stays
-UDS.1 <- simGG(Sim3.1, alpha = 0.3,
-               xlab = "\n Unified Democracy Score", ribbons = TRUE) +
-               scale_x_continuous(breaks = c(-2, -1, 0))
+polity2.1 <- simGG(Sim3.1, alpha = 0.3,
+               xlab = "\n Polity Score", ribbons = TRUE)
 
-UDS.2 <- simGG(Sim3.2, alpha = 0.3,
-               xlab = "", ylab = "", ribbons = TRUE) + 
-               scale_x_continuous(breaks = c(0, 0.5, 1))
+polity2.2 <- simGG(Sim3.2, alpha = 0.3,
+               xlab = "", ylab = "", ribbons = TRUE)
 
-UDS.3 <- simGG(Sim3.3, alpha = 0.3,
-               xlab = "", ylab = "", ribbons = TRUE) + 
-               scale_x_continuous(breaks = c(1, 1.5, 2))
-
-pdf(file = "~/Dropbox/AMCProject/figure/UDSHazardRatio.pdf", width = 12, paper = "a4r")
-  grid.arrange(UDS.1, UDS.2, UDS.3, ncol = 3)
+pdf(file = "~/Dropbox/AMCProject/figure/polity2HazardRatio.pdf", width = 12, paper = "a4r")
+  grid.arrange(polity2.1, polity2.2, ncol = 2)
 dev.off()
 
 ##################### Polarization/Checks Marginal Effect #########
@@ -335,40 +327,36 @@ dev.off()
 
 ##################### Econ Inst. Quality/Democarcy Marginal Effect #########
 
-# Simulate Marginal Effects
-# Sim5 <- coxsimInteract(MD10, b1 = 'EconReScale', b2 = 'UDS', 
-#                       qi = 'Marginal Effect', X2 = seq(-2, 2, 0.1), ci = 0.95)
-
-Sim6 <- coxsimInteract(MD10Plot, b1 = 'UDS', b2 = 'EconReScale', 
+Sim6 <- coxsimInteract(MD10Plot, b1 = 'polity2', b2 = 'EconReScale', 
                        qi = 'Marginal Effect', X2 = seq(-30, 40, 1), ci = 0.95)
 
 # Plot and save
 pdf(file = "~/Dropbox/AMCProject/figure/QualDemMarg.pdf")
 
-  simGG(Sim6, ylab = "Marginal Effect of Unified Democracy Score\n",
+  simGG(Sim6, ylab = "Marginal Effect of Polity\n",
       xlab = "\nEconomic Institutional Quality", ribbon = TRUE, alpha = 0.3)
 
   # grid.arrange(UEInter1, UEInter2, ncol = 2)
 dev.off()
 
 ##################### Hazard Ratio #########
-Sim7 <- coxsimInteract(MD10, b1 = 'EconReScale', b2 = 'UDS', 
-                       qi = 'Hazard Ratio', X1 = seq(16, 40, by = 1), 
-                       X2 = c(0, 2), ci = 0.95, spin = TRUE)
+Sim7 <- coxsimInteract(MD10, b1 = 'EconReScale', b2 = 'polity2', 
+                       qi = 'Hazard Ratio', X1 = seq(16, 40, by = 0.5), 
+                       X2 = c(-2, 10), ci = 0.95, spin = TRUE)
 
 Sim7$X2 <- as.character(Sim7$X2)
-Sim7$X2[Sim7$X2 == '0'] <- 'Average (0)'
-Sim7$X2[Sim7$X2 == '2'] <- 'High (1)'
+Sim7$X2[Sim7$X2 == '-2'] <- 'Lower (-2)'
+Sim7$X2[Sim7$X2 == '10'] <- 'High (10)'
 
 pdf(file = "~/Dropbox/AMCProject/figure/EconUDSHR.pdf")
   simGG(Sim7, xlab = "\nEconomic Institutional Quality", ribbons = TRUE,
-        leg.name = "Democracy (UDS)")
+        leg.name = "Democracy (Polity)")
 dev.off()
 
 ############## Plot Matrix
 
 pdf(file = "~/Dropbox/AMCProject/figure/PlotMatrix.pdf")
   Vars <- c('SystemicCrisisLag3', 'TotalReservesGDPLog', 'CvHOwnPerc', 'EconReScale',
-            'UDS', 'polariz', 'checks', 'GDPperCapitaLog', 'GDPCurrentUSDLog')
+            'polity2', 'polariz', 'checks', 'GDPperCapitaLog', 'GDPCurrentUSDLog')
   plotmatrix(Data[, Vars])
 dev.off()
